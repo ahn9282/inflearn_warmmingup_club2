0580bce15ebf9c4f0dc2046a679a82d1978f185d
cajunSub <junsyub@gmail.com>
2024-10-15 16:04:40 +0900

    테스트 공부 강의 프로젝트 추가

---
+++ practical_testing/test_study/src/test/java/test_study/cafekiosk/TestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package test_study.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
